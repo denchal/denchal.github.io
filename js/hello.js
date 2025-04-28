032c36ae713284b2ff97ae3cd4d2bb2889601d74
MichalGomulczak <michal.gomulczak1@gmail.com>
2025-04-28 18:13:38 +0200

    Add landing page

---
+++ js/hello.js
@@ -0,0 +1,7 @@
+document.getElementById('register').addEventListener('click', async () => {
+  window.location.href = 'public/register.html';
+});
+
+document.getElementById('userlogin').addEventListener('click', async () => {
+  window.location.href = 'public/login.html';
+});
